639e5b44b57225e240e521350ef89e50a1b3d09a
ToshihitoKon <toshihito.kon@gmail.com>
2018-08-27 18:59:44 +0900

    add permanent login session

--- myapp/app/helpers/sessions_helper.rb
+++ myapp/app/helpers/sessions_helper.rb
@@ -5,9 +5,24 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # permanent user session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # return current login user
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user User.find_by(id: cookies.signed[:user_id])
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # return login status (boolean)
@@ -15,8 +30,16 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # remove permanent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # logout current user
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! }
 
   validates :name,  presence: true, length: { maximum: 50 }
@@ -12,10 +13,34 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
 
-  # return hash of argumented strings
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  class << self
+    # return hash of argumented strings
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # return random token
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  # remember user in database for continued session
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # return true when argumented token matching remember token
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # remove user's login data
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
