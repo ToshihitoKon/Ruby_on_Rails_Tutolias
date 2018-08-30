e8b4702b737d4690b36cd367a3edc88228061678
ToshihitoKon <toshihito.kon@gmail.com>
2018-08-30 20:02:37 +0900

    add activated action and tests

--- myapp/test/integration/users_index_test.rb
+++ myapp/test/integration/users_index_test.rb
@@ -4,6 +4,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "index including pagination" do
@@ -14,5 +15,24 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
+  end
+
+  test "index not listed unactivated user" do
+    @other_user.update_attribute(:activated, false)
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'a[href=?]', user_path(@other_user), count: 0
+  end
+  
+  test "can access to activated users page" do
+    get users_path(@other_user)
+    assert_redirected_to login_path
+  end
+
+  test "cannot access to not activated users page" do
+    @other_user.update_attribute(:activated, false)
+    get users_path(@other_user)
+    assert_redirected_to login_path
   end
 end
